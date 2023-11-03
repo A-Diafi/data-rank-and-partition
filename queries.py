2fcfb345ff6d669871a5e06c2b5473db01c3c0f6
Amin Diafi <amindiafi.ad@gmail.com>
2023-11-03 22:12:01 +0000

    Queries.py

--- queries.py
+++ queries.py
@@ -1,8 +1,39 @@
 # pylint:disable=C0111,C0103
+import sqlite3
+conn = sqlite3.connect('data/ecommerce.sqlite')
+c = conn.cursor()
 
 def order_rank_per_customer(db):
-    pass  # YOUR CODE HERE
+# - Implement `order_rank_per_customer` to rank the orders of each customer according to the order date.
+# - For each customer, the orders should be ranked in the chronological order.
+# - This function should return a list of tuples like (`OrderID`, `CustomerID`, `OrderDate`, `OrderRank`).
+    query = """SELECT Orders.OrderID, orders.CustomerID , orders.OrderDate,
+    RANK() OVER (
+    PARTITION BY orders.CustomerID
+    ORDER BY orders.OrderDate
+    ) AS 'order_rank_per_customer'
+    FROM Orders
+    """
+    c.execute(query)
+    rows = c.fetchall()
+    return rows
+
+
 
 
 def order_cumulative_amount_per_customer(db):
-    pass  # YOUR CODE HERE
+# - Implement `order_cumulative_amount_per_customer` to compute the cumulative amount (in USD) of the orders of each customer according to the order date.
+# - For each customer, the orders should be ranked in the chronological order.
+# - This function should return a list of tuples like (`OrderID`, `CustomerID`, `OrderDate`, `OrderCumulativeAmount`).
+    query = """
+    SELECT DISTINCT orders.OrderID, orders.CustomerID , orders.OrderDate,
+    SUM(OrderDetails.UnitPrice * OrderDetails.Quantity) OVER(
+    PARTITION BY orders.CustomerID
+    ORDER BY orders.OrderDate
+    ) AS 'order_cumulative_amount_per_customer'
+    FROM Orders
+    LEFT JOIN OrderDetails ON OrderDetails.OrderID = Orders.OrderID
+    """
+    c.execute(query)
+    rows = c.fetchall()
+    return rows
